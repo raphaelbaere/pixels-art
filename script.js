97ed8e86ec39a54fdb8b07254223b3b1b89f9b79
raphaelbaere <raphaelbaere@id.uff.br>
2022-08-31 16:38:08 -0300

    Teste do requisito 5

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+window.addEventListener('load', () => {
+  const colors = document.querySelectorAll('.color');
+  const hexColors = JSON.parse(localStorage.getItem('colorPalette'));
+  for (let index = 1; index < colors.length; index += 1) {
+    switch (index) {
+    case 1:
+      colors[index].style.backgroundColor = hexColors[index];
+      break;
+    case 2:
+      colors[index].style.backgroundColor = hexColors[index];
+      break;
+    case 3:
+      colors[index].style.backgroundColor = hexColors[index];
+      break;
+    default:
+    }
+  }
+});
+
 function generateColor() {
   const letters = '0123456789ABCDEF';
   let color = '#';
@@ -10,8 +29,11 @@ function generateColor() {
 
 const buttonRandomColor = document.querySelector('#button-random-color');
 buttonRandomColor.addEventListener('click', () => {
+  const colorPalette = {};
   const colors = document.querySelectorAll('.color');
   for (let index = 1; index < colors.length; index += 1) {
     colors[index].style.backgroundColor = generateColor();
+    colorPalette[index] = colors[index].style.backgroundColor;
+    localStorage.setItem('colorPalette', JSON.stringify(colorPalette));
   }
 });
